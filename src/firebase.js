82285e86f5a3af630edce4919c178693cb71ceae
Marielo <sapientia.dunkel@hotmail.com>
2023-07-05 16:33:10 +0200

    app recipe 2022

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 let config = {
-  apiKey: "AIzaSyC12qIuttbNFNw24Y9-vYsyCQzhnuFAXwg",
-  authDomain: "db-recipe-52999.firebaseapp.com",
-  databaseURL: "https://db-recipe-52999-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "db-recipe-52999",
-  storageBucket: "db-recipe-52999.appspot.com",
-  messagingSenderId: "1027409485444",
-  appId: "1:1027409485444:web:3e437f8e764906bfac2cd1",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 firebase.initializeApp(config);
